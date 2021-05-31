339ee9855e7848e7e095e8fc37da200cf3d65e3b
Emmanuel Benitez <emmanuel.benitez@gmail.com>
2021-05-31 16:12:01 +0200

    Add BatchFactory and remove Part from BatchingDbContext

--- src/Be.Stateless.BizTalk.Batching.Unit/Unit/BatchingDbContext.cs
+++ src/Be.Stateless.BizTalk.Batching.Unit/Unit/BatchingDbContext.cs
@@ -22,14 +22,6 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
-using System.Xml;
-using Be.Stateless.BizTalk.Message;
-using Be.Stateless.BizTalk.Schema;
-using Be.Stateless.BizTalk.Stream;
-using Be.Stateless.Extensions;
-using Be.Stateless.Xml.Extensions;
-using Microsoft.XLANGs.BaseTypes;
 
 namespace Be.Stateless.BizTalk.Unit
 {
@@ -37,61 +29,7 @@ namespace Be.Stateless.BizTalk.Unit
 	[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "Public API.")]
 	public class BatchingDbContext
 	{
-		#region Nested Type: Part
-
-		public class Part
-		{
-			public string EnvelopeSpecName { get; internal set; }
-
-			public string MessagingStepActivityId { get; internal set; }
-
-			public string Partition { get; internal set; }
-		}
-
-		#endregion
-
-		public static System.IO.Stream CreateReleaseMessageFor<TSchema>(string partition = null)
-			where TSchema : SchemaBase
-		{
-			var builder = new StringBuilder();
-			using (var writer = XmlWriter.Create(builder, new XmlWriterSettings { Indent = false, OmitXmlDeclaration = true, Encoding = Encoding.UTF8 }))
-			{
-				const string prefix = "ns";
-				var schemaMetadata = SchemaMetadata.For<Schemas.Xml.Batch.Release>();
-				var ns = schemaMetadata.TargetNamespace;
-				writer.WriteStartElement(prefix, schemaMetadata.RootElementName, ns);
-				{
-					writer.WriteElementString(prefix, nameof(BatchDescriptor.EnvelopeSpecName), ns, SchemaMetadata.For<TSchema>().DocumentSpec.DocSpecStrongName);
-					if (!partition.IsNullOrWhiteSpace()) writer.WriteElementString(prefix, nameof(BatchDescriptor.Partition), ns, partition);
-				}
-				writer.WriteEndElement();
-			}
-			return MessageBodyFactory.Create<Schemas.Xml.Batch.Release>(builder.ToString()).AsStream();
-		}
-
 		private static SqlConnection Connection => new(ConfigurationManager.ConnectionStrings["TransientStateDb"].ConnectionString);
-
-		public static IEnumerable<Part> Parts
-		{
-			get
-			{
-				using (var connection = Connection)
-				using (var command = new SqlCommand(
-					"SELECT E.EnvelopeSpecName, P.MessagingStepActivityId, P.Partition FROM batch_Parts P INNER JOIN batch_Envelopes E ON P.EnvelopeId = E.Id",
-					connection))
-				{
-					connection.Open();
-					return command.ExecuteReader().Cast<IDataRecord>()
-						.Select(
-							r => new Part {
-								EnvelopeSpecName = (string) r[nameof(Part.EnvelopeSpecName)],
-								MessagingStepActivityId = (string) r[nameof(Part.MessagingStepActivityId)],
-								Partition = (string) r[nameof(Part.Partition)]
-							})
-						.ToArray();
-				}
-			}
-		}
 
 		public static void ClearParts()
 		{
